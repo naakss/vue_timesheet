ea72b68f4a75d643c384f590e2c517989191f104
Sagar Nayak <sagar.nayak@e-gits.com>
2020-09-17 17:53:12 +0200

    added basic timesheet table

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-main>
+      <TimesheetTable/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TimesheetTable from './components/TimesheetTable';
+
+export default {
+  name: 'App',
+
+  components: {
+    TimesheetTable,
+  },
+
+};
+</script>
